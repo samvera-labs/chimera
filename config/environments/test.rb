c7d10573b354289fb968f6d15a87f109cedb0eb3
Fritz Freiheit <fritx@umich.edu>
2019-04-02 13:32:06 -0400

    BLUEDOC-420 - Upgrade to Hyrax version 2.5.0
    * Modified several files based on changes between Hyrax v2.2.4 and v2.5.0

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,7 +6,7 @@ require "devise/fake_auth_header"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  
+
   if Rails.configuration.authentication_method == "umich"
     # Middleware to fake authentication header field that would come from apache. ONLY APPLIES TO UMICH AUTHENTICATION
     config.middleware.use FakeAuthHeader
